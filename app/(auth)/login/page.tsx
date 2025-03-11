26ebfd167432c00566fc4254d196f132deeae954
adindanabilakh <halfsorx@gmail.com>
2025-03-11 23:55:17 +0700

    first commit

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -50,7 +50,7 @@ function LoginForm() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/umkm/login`,
+        `http://127.0.0.1:8080/api/umkm/login`,
         {
           method: "POST",
           headers: {
